feca7842d2de3ad9e565ab6062cdc266c351d98d
Joel Bradshaw <cincodenada@gmail.com>
2016-12-24 11:20:07 -0800

    Refactor node_pool
    Add a node object, stop passing it all around the damn place

--- exits.py
+++ exits.py
@@ -4,10 +4,23 @@ import math
 tree = ET.parse('motorway.osm')
 root = tree.getroot()
 
+class Node:
+    def __init__(self, xmlobj):
+        self.id = xmlobj.get('id')
+        self.lat = xmlobj.get('lat')
+        self.lon = xmlobj.get('lon')
+        type = xmlobj.find('./tag[@k="highway"]')
+        if(type == 'motorway_junction'):
+            self.name = xmlobj.find('./tag[@k="ref"]').get('v')
+        else:
+            self.name = None
+
 class HwySeg:
     lane_keys = ['turn','hov','hgv','bus','motor_vehicle','motorcycle']
-    def __init__(self, el):
+
+    def __init__(self, el, node_pool):
         self.el = el
+        self.node_pool = node_pool
 
         self.id = int(self.el.attrib.get('id'))
 
@@ -33,6 +46,13 @@ class HwySeg:
         self.exits = []
         self.entrances = []
 
+    def describe_link(self, trunk):
+        # TODO: Factor out nodecoords here
+        fromto = 'to' if trunk.get_link_type(self) == 'exit' else 'from'
+        side = trunk.get_side(nodecoords, self)
+        dest = self.dest if self.dest else '???'
+        return '{} {} {} ({})'.format(self.name, fromto, dest, side)
+
     def get_tag(self, k):
         tagel = self.el.find("./tag[@k='{}']".format(k))
         # I'm not sure why this is necessary?
@@ -44,17 +64,18 @@ class HwySeg:
         else:
             return None
 
-    def get_ang(self, node_pool, link_type, start_node = None):
+    def get_ang(self, link_type, start_node = None):
         rev = (link_type == 'entrance')
         # Set default start_node depending on rev
         if(start_node is None):
             start_node = len(self.nodes)-1 if rev else 0
 
         if(len(self.nodes) >= 2):
-            p = [node_pool[n] for n in (self.nodes[start_node:start_node-2:-1] if rev else self.nodes[start_node:start_node+2])]
-            return math.atan2(p[1][0] - p[0][0], p[1][1] - p[0][1])
+            p = [self.node_pool[n] for n in (self.nodes[start_node:start_node-2:-1] if rev else self.nodes[start_node:start_node+2])]
+            return math.atan2(p[1].lon - p[0].lon, p[1].lat - p[0].lat)
         else:
             return None
+
 
     def get_link_type(self, link):
         if((link.start in self.nodes) and (link.start != self.end)):
@@ -64,14 +85,14 @@ class HwySeg:
         else:
             return None
 
-    def get_rel_ang(self, node_pool, link):
+    def get_rel_ang(self, link):
         link_type = self.get_link_type(link)
         if(link_type is None):
             return None
 
         center_id = link.start if link_type == 'exit' else link.end
         start_node = self.nodes.index(center_id)
-        diff = link.get_ang(node_pool, link_type) - self.get_ang(node_pool, link_type, start_node)
+        diff = link.get_ang(link_type) - self.get_ang(link_type, start_node)
 
         if(abs(diff) == 180):
             raise ValueError("Exit 180 degrees from road!")
@@ -81,12 +102,12 @@ class HwySeg:
 
         return diff
 
-    def get_side(self, node_pool, link):
+    def get_side(self, link):
         type = self.get_link_type(link)
         if(type is None):
             return None
         else:
-            rel_ang = self.get_rel_ang(node_pool, link)
+            rel_ang = self.get_rel_ang(link)
             if(type == 'entrance'):
                 return 'R' if rel_ang > 0 else 'L'
             else:
@@ -118,8 +139,8 @@ class Hwy:
             self.starts.append(seg)
 
     def add_link(self, link):
-        if(link.start in self.end_idx):
-            self.pool[self.end_idx[link.start]].exits.append(link)
+        if(link.start in self.start_idx):
+            self.pool[self.start_idx[link.start]].exits.append(link)
         if(link.end in self.end_idx):
             self.pool[self.end_idx[link.end]].entrances.append(link)
 
@@ -151,11 +172,14 @@ hwy_end = {}
 links = {}
 
 print("Getting nodes...")
-nodecoords = {int(n.get('id')): (float(n.get('lat')), float(n.get('lon'))) for n in root.iter('node')}
+nodes = {}
+for n in root.iter('node'):
+    curnode = Node(n)
+    nodes[curnode.id] = curnode
 
 print("Getting ways...")
 for way in root.iter('way'):
-    seg = HwySeg(way)
+    seg = HwySeg(way, nodes)
 
     if(seg.type == 'motorway'):
         hwy_segs[seg.id] = seg
@@ -173,22 +197,21 @@ for way in root.iter('way'):
 
 print("Analyzing...")
 
-print(links[452336723].get_ang(nodecoords, True))
-print(hwy_segs[4748960].get_ang(nodecoords, False))
-print(links[436165683].get_ang(nodecoords, False))
+print(links[452336723].get_ang(True))
+print(hwy_segs[4748960].get_ang(False))
+print(links[436165683].get_ang(False))
 
 # L
-print(hwy_segs[452336740].get_side(nodecoords, links[452336723]))
+print(hwy_segs[452336740].get_side(links[452336723]))
 # R
-print(hwy_segs[4748960].get_side(nodecoords, links[436165683]))
+print(hwy_segs[4748960].get_side(links[436165683]))
 # R
-print(hwy_segs[428232211].get_side(nodecoords, links[96260970]))
+print(hwy_segs[428232211].get_side(links[96260970]))
 # L
-print(hwy_segs[14017470].get_side(nodecoords, links[85106512]))
+print(hwy_segs[14017470].get_side(links[85106512]))
 # Breaks, not correct seg
-print(hwy_segs[5130429].get_side(nodecoords, links[85106512]))
+print(hwy_segs[5130429].get_side(links[85106512]))
 
-sys.exit()
 hwys = HwySet(hwy_segs)
 for name in hwy_names:
     hwys.add_hwy(name, hwy_start[name], hwy_end[name])
@@ -206,9 +229,9 @@ for start in hwys.get_hwy('I 5').starts:
         if(curlanes != curseg.lanes or len(curseg.exits) or len(curseg.entrances)):
             lanes = 'H'*curlanes
             if(len(curseg.exits)):
-                lanes += '-> ' + ';'.join([s.dest if s.dest else '???' for s in curseg.exits])
+                lanes += '-> ' + ';'.join([s.describe_link(curseg) for s in curseg.exits])
             if(len(curseg.entrances)):
-                lanes += '<- ???'
+                lanes += '<-' + ';'.join([s.describe_link(curseg) for s in curseg.exits])
             print(lanes)
             curlanes = curseg.lanes
         curseg = curseg.next
